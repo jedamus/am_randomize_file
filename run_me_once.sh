8de7cfe26ce8a44c7faef0494af4291481c9a4de
Leander Jedamus <ljedamus@web.de>
2019-11-21 07:10:38 +0100

    added make distcheck

--- run_me_once.sh
+++ run_me_once.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
 # erzeugt Mittwoch, 20. November 2019 14:15 (C) 2019 von Leander Jedamus
+# modifiziert Donnerstag, 21. November 2019 07:10 von Leander Jedamus
 # modifiziert Mittwoch, 20. November 2019 14:16 von Leander Jedamus
 
 autoreconf --install
 ./configure
 make
 src/am_randomize_file
+make distcheck
 
 # vim:ai sw=2
 
