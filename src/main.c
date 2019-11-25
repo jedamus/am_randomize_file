38095e7e78d9d790a2a9080973e5aa13170bb20e
Leander Jedamus <ljedamus@web.de>
2019-11-25 07:11:24 +0100

    added translate-dir

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /* -*- C -*- */
 /* main.c */
 /* erzeugt Montag, 28. Oktober 2019 13:27 (C) 2019 von Leander Jedamus */
-/* modifiziert Montag, 25. November 2019 07:03 von Leander Jedamus */
+/* modifiziert Montag, 25. November 2019 07:10 von Leander Jedamus */
 /* modifiziert Donnerstag, 21. November 2019 07:20 von Leander Jedamus */
 /* modifiziert Mittwoch, 20. November 2019 13:18 von Leander Jedamus */
 /* modifiziert Dienstag, 12. November 2019 11:52 von Leander Jedamus */
@@ -44,7 +44,7 @@ int main(int argc, char * argv[]) {
   setlocale(LC_MESSAGES, "de_DE");
 
   # bindtextdomain(PACKAGE, LOCALEDIR);
-  bindtextdomain(PACKAGE, ".");
+  bindtextdomain(PACKAGE, "./translate");
   textdomain(PACKAGE);
 
   sscanf(argv[3],"%d",&randomnumber);
