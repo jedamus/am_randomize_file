9faf2bce68b313afd8818904d5f37d82c00e7966
Leander Jedamus <ljedamus@web.de>
2019-11-25 07:17:25 +0100

    moved setlocale and textdomain

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /* -*- C -*- */
 /* main.c */
 /* erzeugt Montag, 28. Oktober 2019 13:27 (C) 2019 von Leander Jedamus */
-/* modifiziert Montag, 25. November 2019 07:10 von Leander Jedamus */
+/* modifiziert Montag, 25. November 2019 07:16 von Leander Jedamus */
 /* modifiziert Donnerstag, 21. November 2019 07:20 von Leander Jedamus */
 /* modifiziert Mittwoch, 20. November 2019 13:18 von Leander Jedamus */
 /* modifiziert Dienstag, 12. November 2019 11:52 von Leander Jedamus */
@@ -35,17 +35,16 @@ int main(int argc, char * argv[]) {
   int randomnumber = 0;
   int zeilen = 0;
 
-  if (argc < 4) {
-    usage(argv[0]);
-  };
-
   setlocale(LC_ALL, "de_DE");
   setlocale(LC_CTYPE, "de_DE");
   setlocale(LC_MESSAGES, "de_DE");
 
-  # bindtextdomain(PACKAGE, LOCALEDIR);
-  bindtextdomain(PACKAGE, "./translate");
+  bindtextdomain(PACKAGE, "../translate");
   textdomain(PACKAGE);
+
+  if (argc < 4) {
+    usage(argv[0]);
+  };
 
   sscanf(argv[3],"%d",&randomnumber);
   srand(randomnumber);
