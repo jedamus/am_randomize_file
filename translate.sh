4a9ad13b6d8739ced3de1ef76355e3dcc6988168
Leander Jedamus <ljedamus@web.de>
2019-11-25 07:09:25 +0100

    added translate.sh

---
+++ translate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# erzeugt Montag, 25. November 2019 07:05 (C) 2019 von Leander Jedamus
+# modifiziert Montag, 25. November 2019 07:07 von Leander Jedamus
+
+translate.pl --file=src/main.c --project=am_randomize_file --version=1.1
+
+# vim:ai sw=2
+
