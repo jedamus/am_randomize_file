bfa43c4fcad8da10a0d0e9eea886ff22d35baa0e
Leander Jedamus <ljedamus@web.de>
2019-11-25 10:58:05 +0100

    several changes to use gettext

--- run_me_once.sh
+++ run_me_once.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
 # erzeugt Mittwoch, 20. November 2019 14:15 (C) 2019 von Leander Jedamus
+# modifiziert Montag, 25. November 2019 10:09 von Leander Jedamus
 # modifiziert Donnerstag, 21. November 2019 07:10 von Leander Jedamus
 # modifiziert Mittwoch, 20. November 2019 14:16 von Leander Jedamus
 
 autoreconf --install
+gettextize -f
+mv po/Makevars.template po/Makevars
 ./configure
 make
 src/am_randomize_file
