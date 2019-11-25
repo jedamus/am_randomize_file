f85a7a779adbd1561c81d2d34303c7168881ecc9
Leander Jedamus <ljedamus@web.de>
2019-11-25 06:46:20 +0100

    added src/gettext.h

---
+++ src/gettext.h
@@ -0,0 +1,17 @@
+/* -*- C -*- */
+/* gettext.h */
+/* erzeugt Montag, 25. November 2019 06:45 (C) 2019 von Leander Jedamus */
+/* modifiziert Montag, 25. November 2019 06:45 von Leander Jedamus */
+
+#ifndef GETTEXT_H
+#define GETTEXT_H 1
+
+#define _(String) (String)
+#define N_(String) String
+#define textdomain(Domain)
+#define bindtextdomain(Package, Directory)
+
+#endif /* GETTEXT_H */
+
+/* vim:set cindent ai sw=2 */
+
