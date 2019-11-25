283c0b5575d2570d28010aa98a777ec6dbd5a154
Leander Jedamus <ljedamus@web.de>
2019-11-25 07:20:44 +0100

    commented out setlocale

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /* -*- C -*- */
 /* main.c */
 /* erzeugt Montag, 28. Oktober 2019 13:27 (C) 2019 von Leander Jedamus */
-/* modifiziert Montag, 25. November 2019 07:16 von Leander Jedamus */
+/* modifiziert Montag, 25. November 2019 07:20 von Leander Jedamus */
 /* modifiziert Donnerstag, 21. November 2019 07:20 von Leander Jedamus */
 /* modifiziert Mittwoch, 20. November 2019 13:18 von Leander Jedamus */
 /* modifiziert Dienstag, 12. November 2019 11:52 von Leander Jedamus */
@@ -35,11 +35,11 @@ int main(int argc, char * argv[]) {
   int randomnumber = 0;
   int zeilen = 0;
 
-  setlocale(LC_ALL, "de_DE");
-  setlocale(LC_CTYPE, "de_DE");
-  setlocale(LC_MESSAGES, "de_DE");
+# setlocale(LC_ALL, "de_DE");
+# setlocale(LC_CTYPE, "de_DE");
+# setlocale(LC_MESSAGES, "de_DE");
 
-  bindtextdomain(PACKAGE, "../translate");
+  bindtextdomain(PACKAGE, "./translate");
   textdomain(PACKAGE);
 
   if (argc < 4) {
