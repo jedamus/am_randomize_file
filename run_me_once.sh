7fd384350fa3cbc5955941fd98f0c62a1a2bc8c1
Leander Jedamus <ljedamus@web.de>
2019-12-02 02:46:22 +0100

    added translate.sh to run in run_me_once.sh

--- run_me_once.sh
+++ run_me_once.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Mittwoch, 20. November 2019 14:15 (C) 2019 von Leander Jedamus
+# modifiziert Montag, 02. Dezember 2019 02:45 von Leander Jedamus
 # modifiziert Montag, 25. November 2019 10:09 von Leander Jedamus
 # modifiziert Donnerstag, 21. November 2019 07:10 von Leander Jedamus
 # modifiziert Mittwoch, 20. November 2019 14:16 von Leander Jedamus
@@ -10,7 +11,9 @@ gettextize -f
 mv po/Makevars.template po/Makevars
 ./configure
 make
+./translate.sh
 src/am_randomize_file
+sleep 10
 make distcheck
 
 # vim:ai sw=2
