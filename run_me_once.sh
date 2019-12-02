41798f9efe9fa65957c5dd2f8827c185d30b7c8e
Leander Jedamus <ljedamus@web.de>
2019-12-02 02:53:37 +0100

    minor changes

--- run_me_once.sh
+++ run_me_once.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Mittwoch, 20. November 2019 14:15 (C) 2019 von Leander Jedamus
-# modifiziert Montag, 02. Dezember 2019 02:50 von Leander Jedamus
+# modifiziert Montag, 02. Dezember 2019 02:53 von Leander Jedamus
 # modifiziert Montag, 25. November 2019 10:09 von Leander Jedamus
 # modifiziert Donnerstag, 21. November 2019 07:10 von Leander Jedamus
 # modifiziert Mittwoch, 20. November 2019 14:16 von Leander Jedamus
@@ -10,8 +10,8 @@ autoreconf --install
 gettextize -f
 mv po/Makevars.template po/Makevars
 ./configure
-make translate.sh
 make
+make translate.sh
 ./translate.sh
 src/am_randomize_file
 sleep 10
